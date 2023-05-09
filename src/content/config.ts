8062dd2361679f0ef1256116b508ba23d0a4049c
Kevin Castro <me@kevincastro.dev>
2023-05-09 10:43:04 -0700

    clean up blog index

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,7 @@ import { z, defineCollection } from "astro:content";
 const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
-    summary: z.string().optional(),
+    summary: z.string(),
     publishDate: z.date(),
     heroImage: z
       .object({
