92169990abd4bd2c869ec45e948ab3a4a4c6a00d
Kevin Castro <me@kevincastro.dev>
2023-06-01 20:58:09 -0700

    more skills page cleanup

--- cypress/constants.ts
+++ cypress/constants.ts
@@ -1,11 +1,5 @@
 // routes
-const availablePages = [
-  'HOME',
-  'ABOUT_ME',
-  'BLOG',
-  'PROJECTS',
-  'SKILLS',
-] as const;
+const availablePages = ['HOME', 'ABOUT_ME', 'BLOG', 'PROJECTS'] as const;
 type PageName = (typeof availablePages)[number];
 
 type Page = {
@@ -19,32 +13,30 @@ export const HOME: Page = {
   title: 'Kevin Castro // Portfolio',
   friendlyText: 'Home',
 };
+
 export const ABOUT_ME: Page = {
   route: '/about-me',
   title: 'Kevin Castro // About Me',
   friendlyText: 'About Me',
 };
+
 export const BLOG: Page = {
   route: '/blog',
   title: 'Kevin Castro // Blog',
   friendlyText: 'Blog',
 };
+
 export const PROJECTS: Page = {
   route: '/projects',
   title: 'Kevin Castro // Projects',
   friendlyText: 'Projects',
 };
-export const SKILLS: Page = {
-  route: '/skills',
-  title: 'Kevin Castro // Skills',
-  friendlyText: 'Skills',
-};
+
 export const PAGES: Record<PageName, Page> = {
   HOME,
   ABOUT_ME,
   BLOG,
   PROJECTS,
-  SKILLS,
 };
 
 // content collections
