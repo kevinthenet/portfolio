efdb3739649911661bf6ef31d7d94adc798f6e0c
Kevin Castro <me@kevincastro.dev>
2023-06-01 19:39:55 -0700

    setup continuous deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,9 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://kevincastro.dev",
   integrations: [react()],
   markdown: {
     wrap: true,
-  }
+  },
 });
