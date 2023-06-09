7c2f6a34622e5b75e898dd87fd214ea7933066af
Kevin Castro <me@kevincastro.dev>
2023-06-09 09:12:48 -0700

    remove react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kevincastro.dev',
-  integrations: [react()],
   markdown: {
     shikiConfig: {
       wrap: true,
-    }
+    },
   },
 });
